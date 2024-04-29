98616af25eb93d7c1a5cf6a63743acb182a2ac45
Adrianmarinb <e.marinadrian@go.ugr.es>
2024-04-29 12:23:36 +0200

    Mejoras

---
+++ Code/numerical_calc.cpp
@@ -0,0 +1,292 @@
+#include <math.h>
+#include <iostream>
+# include <fstream>
+# include <stdlib.h>
+
+using namespace std;
+const int N=100;
+const int M=N-2;
+const double PI = 3.14159265358979323846264;
+
+void iniciar(double u[N][N], double v[N][N], int N, string mode){
+    if(mode == "zeros")
+    {
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++)
+            {
+                u[i][j]=0;
+                v[i][j]=0;
+            }
+        }
+    }
+
+    if(mode == "centro")
+    {
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++)
+            {
+                u[i][j]=0;
+                v[i][j]=0;
+            }
+        }
+
+        for(int i=N/2 - 30; i<N/2 + 30; i++){
+            for(int j=N/2 - 30; j<N/2 + 30; j++)
+            {
+                u[i][j]=1;
+                v[i][j]=0;
+            }
+        }
+    }
+
+    if(mode == "esquina")
+    {
+        for(int i=0; i<N/4; i++){
+            for(int j=0; j<N/4; j++)
+            {
+                u[i][j]=1;
+                v[i][j]=0;
+            }
+        }
+
+        for(int i=N/4; i<N; i++){
+            for(int  j=N/4; j<N; j++)
+            {
+                u[i][j]=0;
+                v[i][j]=0;
+            }
+        }
+    }
+
+    if(mode == "mitad")
+    {
+        for(int i=0; i<N/2.0; i++){
+            for(int j=0; j<N; j++)
+            {
+                u[i][j]=1;
+                u[N-i-1][j]=0;
+                v[i][j]=0;
+                v[N-i-1][j]=1;
+            }
+        }
+    }
+
+    if(mode == "seno")
+    {
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++)
+            {
+                u[i][j]= pow(sin( ((i + j) * 2.0 * PI) / N), 2);
+                v[i][j]= pow(cos( ((i + j) * 2.0 * PI) / N), 2);
+            }
+        }
+    }
+    
+    if(mode == "random")
+    {
+        for (int i = 0; i < N; ++i) 
+        {
+            for (int j = 0; j < N; ++j) 
+            {
+                u[i][j] = rand() / (double)(RAND_MAX + 1.0);
+                v[i][j] = rand() / (double)(RAND_MAX + 1.0);
+            }
+        }
+    }
+
+    
+}
+
+void copiar(double a[N][N], double b[N][N], int N){
+    for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+            a[i][j]=b[i][j];
+            }
+        }
+}
+
+void cyclic_thomas(double x[N], double a, double b) {
+    
+    double alpha = a;
+    double beta = a;
+    double cmod[N], u[N];
+    double gamma = -b;
+    double m;
+    double fact;
+
+    cmod[0] = alpha / (b - gamma);
+    u[0] = gamma / (b - gamma);
+    x[0] = x[0] / (b - gamma);
+
+
+    for (int i = 1; i + 1 < N; i++) {
+        m = 1.0 / (b - a * cmod[i - 1]);
+        cmod[i] = a * m;
+        u[i] = (0.0f  - a * u[i - 1]) * m;
+        x[i] = (x[i] - a * x[i - 1]) * m;
+    }
+
+    m = 1.0 / (b - alpha * beta / gamma - beta * cmod[N - 2]);
+    u[N - 1] = (alpha    - a * u[N - 2]) * m;
+    x[N - 1] = (x[N - 1] - a* x[N - 2]) * m;
+
+    for (int i = N - 2; i >= 0; i--) {
+        u[i] -= cmod[i] * u[i + 1];
+        x[i] -= cmod[i] * x[i + 1];
+    }
+
+    fact = (x[0] + x[N - 1] * beta / gamma) / (1.0 + u[0] + u[N - 1] * beta / gamma);
+
+    for (int i = 0; i < N; i++){
+        x[i] -= fact * u[i];
+    }
+}
+
+void ADI(double u[N][N], double v[N][N],double t,double l,double D1,double D2,double C1,double C2){
+
+    double x1[N][N],x2[N][N],y1[N][N],y2[N][N],a,b,d[N];
+
+
+    //Calculo u_n+1
+
+    a=-D1*t/pow(l,2);
+    b=(1-2*a);
+
+    for(int j=0;j<N;j++){
+        for(int i=0;i<N;i++){
+            d[i]=C1*t-a*(u[i][(j+1)%N]+u[i][(N+j-1)%N])+(1+2*a-t*(C2+1))*u[i][j]+t*pow(u[i][j],2)*v[i][j];
+        }
+        cyclic_thomas(d,a,b);
+        for(int i=0;i<N;i++){
+            x1[i][j]=d[i];
+        }
+    }
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            d[j]=C1*t-a*(x1[(i+1)%N][j]+x1[(N+i-1)%N][j])+(1+2*a)*x1[i][j]-t*(C2+1)*u[i][j]+t*pow(u[i][j],2)*v[i][j];
+
+        }
+        cyclic_thomas(d,a,b);
+        for(int j=0;j<N;j++){
+            x2[i][j]=d[j];
+        }
+    }    
+   
+
+    //Calculo v_n+1
+    a=-D2*t/pow(l,2);
+    b=(1-2*a);
+
+
+    for(int j=0;j<N;j++){
+        for(int i=0;i<N;i++){
+            d[i]=-a*(v[i][(j+1)%N]+v[i][(N+j-1)%N])+(1+2*a)*v[i][j]+t*C2*u[i][j]-t*pow(u[i][j],2)*v[i][j];
+
+        }
+        cyclic_thomas(d,a,b);
+        for(int i=0;i<N;i++){
+            y1[i][j]=d[i];
+        }
+    }
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            d[j]=-a*(y1[(i+1)%N][j]+y1[(N+i-1)%N][j])+(1+2*a)*y1[i][j]+t*C2*u[i][j]-t*pow(u[i][j],2)*v[i][j];
+        }
+        cyclic_thomas(d,a,b);
+        for(int j=0;j<N;j++){
+            y2[i][j]=d[j];
+        }
+    }
+
+        copiar(u,x2,N);
+        copiar(v,y2,N);
+
+}
+
+void crear_fichero(string nombre)
+{
+    ofstream salida(nombre);
+    salida << "";
+    salida.close();
+}
+
+void comparativa_rgb(double u[][N], double v[][N], double p_u[][N], double p_v[][N])
+{
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N; j++)
+        {
+            if(u[i][j] - v[i][j] >= 0)
+            {
+                p_u[i][j] = (u[i][j] - v[i][j]) + 0.3;
+                p_v[i][j] = 0.35; // (u[i][j] - v[i][j]);
+            }
+            else
+            {
+                p_v[i][j] = (v[i][j] - u[i][j]) + 0.3;
+                p_u[i][j] = 0.35; // (v[i][j] - u[i][j]);
+            }
+        }
+    }
+}
+
+void escribir_datos(double u[][N],double v[][N], int N)
+{
+    ofstream salida1("Chemical_oscillations_u.txt", ios::app), salida2("Chemical_oscillations_v.txt", ios::app);
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N-1; j++)
+        {
+            salida1 << u[i][j] << ",";
+        }
+        salida1 << u[i][N-1] <<"\n";
+    }
+    salida1 << "\n";
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<N-1; j++)
+        {
+            salida2 << v[i][j] << ",";
+        }
+        salida2 << v[i][N-1] <<"\n";
+    }
+
+    salida2 << "\n";
+    salida1.close();
+    salida2.close();
+}
+
+int main()
+{
+    double u[N][N],v[N][N], p_u[N][N],p_v[N][N];
+    double t,l,D1,D2,C1,C2;
+
+    // mode = "zeros", "random", "centro", "mitad", "seno", "esquina"
+    string mode = "random";
+
+    t=0.0001;
+    l=0.01;
+    D1=0.1/4.0;
+    D2=0.1/4.0; 
+    C1=0.6;
+    C2=0.2;
+    crear_fichero("Chemical_oscillations_u.txt");
+    crear_fichero("Chemical_oscillations_v.txt");
+    iniciar(u,v,N,mode);
+
+    for(int n=0;n<2000;n++){
+        if( (n%10) == 0)
+            {
+            comparativa_rgb(u,v,p_u,p_v);
+            escribir_datos(p_u,p_v,N);
+            }
+        ADI(u,v,t,l,D1,D2,C1,C2);
+    }
+    comparativa_rgb(u,v,p_u,p_v);
+    escribir_datos(p_u,p_v,N);;
+
+    return 0;
+
+}
--- Code/visualization.py
+++ Code/visualization.py
@@ -6,7 +6,7 @@ import io
 
 # Parameters
 file_out = "chemical_reactions"  # Output file name (without extension)
-interval = 10  # Time between frames in milliseconds
+interval = 50  # Time between frames in milliseconds
 save_to_file = True  # False: show the animation on screen, True: save to a file
 dpi = 300  # Output video quality (dots per inch)
 
@@ -23,53 +23,80 @@ def load_data(file_name):
     return frames_data
 
 # Load data from the "u" file
-frames_data_u = load_data("Chemical_oscillations_u.txt")
+f_u = load_data("Chemical_oscillations_u.txt")
 
 # Load data from the "v" file
-frames_data_v = load_data("Chemical_oscillations_v.txt")
+f_v = load_data("Chemical_oscillations_v.txt")
 
 # Create matrices for RGB color coding
-nframes = min(len(frames_data_u), len(frames_data_v))
+nframes = min(len(f_u), len(f_v))
 
 # Initialize the frames_data list
 frames_data = []
 
 # Create an array of zeros with the specified shape
-size = len(frames_data_u[0])
-zeros_array = np.zeros((size,size))
+size = len(f_u[0])
+zeros_array = 1 - np.zeros((size,size))
 
 # Iterate over each frame
-for j_frame in range(nframes):
+for j in range(nframes):
     # Stack the three arrays along the last axis
-    disordered_array = np.stack((frames_data_v[j_frame], zeros_array, frames_data_u[j_frame]), axis=-1)
+    
+    max_f_u = np.max(f_u[j])        
+    max_f_v = np.max(f_v[j])     
+    min_f_u = np.min(f_u[j])        
+    min_f_v = np.min(f_v[j])   
+    max_f = np.max([max_f_u, max_f_v])
+    
+    
+    if ((max_f_v - min_f_v) <= 0.000001) & ((max_f_v) > 0):
+        norm_v = f_v[j] / max_f_v
+    elif (max_f_v) > 0:
+        norm_v = (f_v[j] - min_f_v) / (max_f_v - min_f_v)
+    else:
+        norm_v = f_v[j]
+        
+        
+    if ((max_f_u - min_f_u) <= 0.000001) & ((max_f_u) > 0):
+        norm_u = f_u[j] / max_f_u
+    elif (max_f_u) > 0:
+        norm_u = (f_u[j] - min_f_u) / (max_f_u - min_f_u)
+    else:
+        norm_u = f_u[j]
+    
+    
+    disordered_array = np.stack( ( norm_v, zeros_array, norm_u), axis=-1)
     # Append the stacked array to the frames_data list
     frames_data.append(disordered_array)
     
-maximo = np.max(frames_data)
-minimo = np.min(frames_data)
+#alpha = 1
+    
+#maximo = alpha * np.max(frames_data)
 
 # Normalize the image data to the range [0, 1]
-frames_data_normalized = [(frame_data) / (maximo) for frame_data in frames_data]
+# frames_data_normalized =[1 - (frame_data) / (maximo) for frame_data in frames_data]
 
 # Create the figure and axis objects
 fig, ax = plt.subplots()
 ax.axis("off")  # Do not show the axes
+
+# IMPORTANTE: AÑADIR _normalizes a frames_data
             
 # Display the first frame
-im = ax.imshow(frames_data_normalized[0])
+im = ax.imshow(frames_data[0])
 
 # Function to update the animation
 def update(j_frame, frames_data_normalized, im):
-    im.set_data(frames_data_normalized[j_frame])
+    im.set_data(frames_data[j_frame])
     return im,
 
 # Create the animation
-animation = FuncAnimation(fig, update, fargs=(frames_data_normalized, im), frames=nframes, blit=True, interval=interval)
+animation = FuncAnimation(fig, update, fargs=(frames_data, im), frames=nframes, blit=True, interval=interval)
 
 # Save or show the animation
 if save_to_file:
     animation.save("{}.mp4".format(file_out), dpi=dpi)
-
+'''
 else:
     plt.show()
-
+'''
